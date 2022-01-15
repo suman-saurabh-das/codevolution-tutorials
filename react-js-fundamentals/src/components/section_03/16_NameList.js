6ea814ba01b373aec1ceb49e656973e921b527cd
Suman Saurabh Das <dsumansaurabh@gmail.com>
2022-01-15 12:51:05 +0530

    14. Importance of using keys while rendering list

--- react-js-fundamentals/src/components/section_03/16_NameList.js
+++ react-js-fundamentals/src/components/section_03/16_NameList.js
@@ -12,6 +12,13 @@ const NOTES = `
 - We can simplify the return statement by moving out the list rendering logic. e.g. nameList
   - Rendering an array of object with multiple properties.
   - For this the best practice is to create a component which is only responsible for displaying the data and sending the data as props from the parent component (separation of concerns).
+
+- In console we get a warning saying that "Each child in a list should have a unique "key" prop".
+  - In simpler words, each item in the list rendered using map should have a prop called key and the value to that prop should be unique in the list.
+  - Key prop need not be an id always, it can be anything as long as it is unique.
+  - Key props are not accessible in the child components. We will get this warning - "Person: 'key' is not a prop. Trying to access it will result in 'undefined' being returned."
+
+- Importance of key: Keys help react identify which items in the list have changed, added or removed and plays a crucial role in handling UI updates efficiently.
 `;
 
 function NameList() {
@@ -34,7 +41,18 @@ function NameList() {
   //   </h2>
   // ));
 
-  const personList = persons.map((person) => <Person person={person} />);
+  // const personList = persons.map((person) => <Person person={person} />);
+  // return (
+  //   <div>
+  //     <ReactMarkdown>{NOTES}</ReactMarkdown>
+  //     <hr />
+  //     {personList}
+  //   </div>
+  // );
+
+  const personList = persons.map((person) => (
+    <Person key={person.id} person={person} />
+  ));
   return (
     <div>
       <ReactMarkdown>{NOTES}</ReactMarkdown>
