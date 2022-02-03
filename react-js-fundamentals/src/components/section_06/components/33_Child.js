22c2ff6729d805e79501a59c9dd65f566672599e
Suman Saurabh Das <dsumansaurabh@gmail.com>
2022-02-03 23:29:24 +0530

    34. Passing data using Context 2

--- react-js-fundamentals/src/components/section_06/components/33_Child.js
+++ react-js-fundamentals/src/components/section_06/components/33_Child.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import GrandChild from "./33_GrandChild";
+import UserContext from "../33_UserContext";
 
 class Child extends Component {
+  // We can also replace the Child.contextType = UserContext by the following
+  static contextType = UserContext;
   render() {
-    return <GrandChild />;
+    return (
+      <div>
+        <h3>Child component - Name {this.context}</h3>
+        <GrandChild />
+      </div>
+    );
   }
 }
 
+// Child.contextType = UserContext
 export default Child;
