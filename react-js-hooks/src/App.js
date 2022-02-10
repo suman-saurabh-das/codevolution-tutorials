ced0a6f9726cf779f0776d0496aeb69bb390e1c7
Suman Saurabh Das <dsumansaurabh@gmail.com>
2022-02-10 10:57:21 +0530

    01. React.js Hooks - Created a react-app

---
+++ react-js-hooks/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+
+// pages
+import Router from "./pages/Router";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <h1>
+          <Link to={"/"}>React.JS Hooks</Link>
+        </h1>
+        <div className="app-container">
+          <Routes>
+            <Route path="/" element={<Router />} />
+            {/* Section 01 */}
+            <></>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-js-hooks/src/pages/Router.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Router() {
+  return (
+    <div>
+      <details open>
+        <summary className="font-bold text-lg">Section 01</summary>
+        <ul>
+          <li><Link to={"/"}></Link></li>
+        </ul>
+      </details>
+    </div>
+  );
+}
+
+export default Router;
