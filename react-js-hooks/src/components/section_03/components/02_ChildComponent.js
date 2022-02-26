c736c4c01f898cfa8001f0c39268100b51544a32
Suman Saurabh Das <dsumansaurabh@gmail.com>
2022-02-26 22:42:10 +0530

    18. useContext hook (consuming data using hook)

--- react-js-hooks/src/components/section_03/components/02_ChildComponent.js
+++ react-js-hooks/src/components/section_03/components/02_ChildComponent.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import GrandChildComponent from "./03_GrandChildComponent";
+import { UserContext, WorkContext } from "../14_DataPassingUsingContext";
+import ReactMarkdown from "react-markdown";
+
+const NOTES = `
+## useContext: Consuming data using useContext hook
+
+- While using useContext() hook, the process of creating the context and passing the data from the parent component to the child component remains same. The useContext() hook only makes the consumption of data simpler.
+
+#### Steps to implement useContext() hook -
+- import useContext() from React.
+- import the necessary context.
+- Call the useContext() function passing in the context as its argument.
+- useContext() returns the context value which can be stored and accessed via a variable.
+`;
 
 function ChildComponent() {
+  const user = useContext(UserContext);
+  const work = useContext(WorkContext);
   return (
     <div>
+      <ReactMarkdown>{NOTES}</ReactMarkdown>
+      <hr />
       <GrandChildComponent />
+      <h4>
+        Username: {user} | Nature of Work: {work}
+      </h4>
     </div>
   );
 }
